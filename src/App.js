2cc12fe5f5fad4c7122280f447fca66482be80c5
Dalton Meyer <dalton.meyer94@gmail.com>
2020-07-05 20:54:37 -0500

    tested spike

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Iframe from 'react-iframe'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Iframe url="https://www.jftna.org/jft/"
+        width="100%"
+        height="650px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
     </div>
   );
 }
